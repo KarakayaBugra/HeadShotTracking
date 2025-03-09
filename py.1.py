84f7d47d79f9f90c1d37ed652ecb9455e86fa1b7
Tarık Buğra Karakaya <tarik_bugra_77@hotmail.com>
2025-03-10 00:20:38 +0300

    Tracking raised

--- py.1.py
+++ py.1.py
@@ -18,7 +18,7 @@ while True:
         print(cx,cy)
         cv2.line(img,(0,cy-80),(640,cy-80),(),6)
         cv2.line(img, (cx,0), (cx, 480), (), 6)
-        cv2.circle(img,(cx,cy-80),9,(255,0,0),cv2.FILLED)
+        cv2.circle(img,(cx,cy-70),9,(255,0,0),cv2.FILLED)
         cv2.circle(img,(cx,cy-80),26,(0,0,250),3)
         cvzone.putTextRect(img,f'({cx},{cy-80})',(cx+60,cy-100),1.6,2,colorT=(255,255,255),colorR=(255,0,0),border=3,colorB=())
         cvzone.putTextRect(img,'Target Locked!!',(15,40),1.6,2,colorT=(255,255,255),colorR=(0,255,0),border=3,colorB=())
